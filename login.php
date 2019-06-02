7ed839db73f009adeae78f33ce733065638479cd
Bryan <bigbryan@qq.com>
2019-06-02 11:18:16 +0800

    enter

--- login.php
+++ login.php
@@ -17,10 +17,8 @@
 
   <?php
     require('php/dbConnection.php');
-
     $error_msg="";
-    if(isset($_POST['submit']))
-      {//检查是否提交
+    if(isset($_POST['submit'])){//检查是否提交
       //连接数据库
           $dbc=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
           mysqli_query($dbc,'set names utf8');
@@ -28,40 +26,51 @@
           $user_userid = mysqli_real_escape_string($dbc,trim($_POST['userid']));
           $user_password = mysqli_real_escape_string($dbc, trim($_POST['password']));
           //使用函数预防数据库攻击
-          if(!empty($user_userid) && !empty($user_password))
-          {//检查用户信息是否为空
+          if(!empty($user_userid) && !empty($user_password)){//检查用户信息是否为空
           //判断用户身份
-                if($_POST['choice'] == 'option_std')
-                {//学生
+                if($_POST['choice'] == 'option_std'){//学生
                       $query = "
-                             SELECT stu_id,stu_paw,name,sex,class FROM stu_info WHERE id = '$user_userid'
-                             AND password = '$user_password'
+                             SELECT * FROM stuinfo WHERE stuId = '$user_userid'
+                             AND stuPaw = '$user_password'
                              ";
                 }
+                else if($_POST['choice'] == 'option_tea'){//教师
+                      $query = "
+                            SELECT * FROM teainfo WHERE teaId = '$user_userid'
+                            AND teaPaw = '$user_password';
+                      ";
+                }
+                else if($_POST['choice'] == 'option_admin'){//管理员
+                  $query = "
+                        SELECT * FROM admininfo WHERE id = '$user_userid'
+                        AND password = '$user_password';
+                  ";
+            }
+               
+
         //获取数据
                 $data=mysqli_query($dbc,$query);
                 $row=mysqli_fetch_array($data);
-                if(mysqli_num_rows($data) >= 1)
-                {
+                if(mysqli_num_rows($data) >= 1){
                  //判断用户身份，跳转至相应页面
-                      if($_POST['choice']=='option_std')
-                      {//学生
-                            
-                            $student_url = 'http://'. $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 'php/student/student_page.php';
+                      if($_POST['choice']=='option_std'){//学生
+                            $student_url = 'http://'. $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/php/student_page.php';
                             header('Location:'.$student_url);
                             //跳转页面
                       }
+                      else if($_POST['choice']=='option_tea'){//教师
+                            $teacher_url = 'http://'. $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/php/teacher_page.php';
+                            header('Location:'.$teacher_url);
+                      }
+                      else if($_POST['choice']=='option_admin'){//管理员
+                        $admin_url = 'http://'. $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/php/admin_page.php';
+                        header('Location:'.$admin_url);
+                  }
                 }
-                else
-                {
+                else{
                       $error_msg='账号或密码不正确';
                       echo '<p class="error">' . $error_msg . '</p>';
                 }
-          }
-          else
-          {
-            $error_msg='请输入账号密码';
-            echo '<p class="error">' . $error_msg . '</p>';
           }
         }
   ?>
@@ -73,8 +82,13 @@
 
 
     <div class="container">
-      <form class="form-signin" id="form-test">
+      <form class="form-signin" id="form-test" enctype="multipart/form-data" action="" method="post">
         <h2>请登录。</h2>
+        <select name="choice" class="form-control">
+          <option value="option_std">学生</option>
+          <option value="option_tea">教师</option>
+          <option value="option_admin">管理员</option>
+        </select>
         <input
           id="account"
           name="userid"
@@ -91,50 +105,8 @@
           placeholder="密码"
           required
         />
-        <select name="choice" class="form-control">
-          <option value="option_std">学生</option>
-          <option value="option_tea">教师</option>
-          <option value="option_admin">管理员</option>
-        </select>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-          </label>
-        </div>
-        <input id="sumit_btn" class="btn btn-lg btn-success btn-block" type="submit" value="登录"/>
-          
+        <input name="submit" id="submit" class="btn btn-lg btn-success btn-block" type="submit" value="登录"/>     
       </form>
-    </div>
-    <!-- 模态框 -->
-    <div
-      class="modal fade bs-example-modal-sm"
-      tabindex="-1"
-      role="dialog"
-      id="myModal"
-    >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-            <h4 class="modal-title">错误提示</h4>
-          </div>
-          <div class="modal-body">
-            <p>用户名或者密码输入错误</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal">
-              确定
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"></script>
     <script
--- php/dbConnection.php
+++ php/dbConnection.php
@@ -2,6 +2,6 @@
   // Define database connection constants
   define('DB_HOST', '127.0.0.1');
   define('DB_USER', 'root');
-  define('DB_PASSWORD', '');
+  define('DB_PASSWORD', '12345678');
   define('DB_NAME', 'db_student_grade');
 ?>
